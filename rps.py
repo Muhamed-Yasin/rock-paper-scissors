5aa1fd217336dda5fc6ae46d367f998db018a847
Muhamed Yasin <56083787+Muhamed-Yasin@users.noreply.github.com>
2022-01-11 10:32:46 +0530

    Update rps.py
    I added comments to make the code more readable

--- rps.py
+++ rps.py
@@ -1,18 +1,21 @@
+# Rps simulator script
 import random
 
-
+# This keeps the game running till we decide to stop playing
 while True:
-   
+    # This is where the computer chooses
     ch = random.randint(1,3)
-    print("Select your choice:")
     
+    # This is where we give the player the choice
+    print("Select your choice:")
     print("1.Rock")
     print("2.Paper")
     print("3.Scissors")
     p1 = int(input("1,2 or 3:"))
 
     print(f"You have chosen {p1}")
-
+    
+    #This decides the winner of the game
     if p1 == ch:
         print("It's a Tie")
     else:
@@ -31,7 +34,12 @@ while True:
                 print("You win.Scissors cut Paper")
             else:
                 print("You lose.Rock smashes Scissors")
+    
+    #Give the player the chance to try again
+    
     print("Try again?")
     try2 = input("y/n")
+    
+    #This is to stop the game
     if try2.lower() != "y":
         break
